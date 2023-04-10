a28c5e562e0797e33b8b801bd91eacc9c295ed41
Ariel Rezin <arielrezin@gmail.com>
2023-04-09 23:14:25 -0500

    Updates

--- RefreshData.js
+++ RefreshData.js
@@ -1,11 +1,17 @@
 const fs = require("fs");
 const csvToJson = require('csvtojson');
-const { element } = require("prop-types");
+
+let treeInfo = {
+    totalNumNodes: 0,
+    numDataRowsProcessed: 0
+}
+
+const fileOutputLocation = "./src/refreshedData/current/"
 
 const getCsv = async () => {
     try {
-        // current from google drive
-        const csvPedigreeUrl = "https://docs.google.com/spreadsheets/d/e/2PACX-1vS_t21NRDGXKya4lVd_Uijhnz-TDwqM36QyYfEncJQO-TCLCNdvT44KTe_ZafSROrJY6DW3YxdAI608/pub?gid=0&single=true&output=csv";
+        // "restructured" sheet from google drive
+        const csvPedigreeUrl = "https://docs.google.com/spreadsheets/d/e/2PACX-1vS_t21NRDGXKya4lVd_Uijhnz-TDwqM36QyYfEncJQO-TCLCNdvT44KTe_ZafSROrJY6DW3YxdAI608/pub?gid=308395222&single=true&output=csv"
 
         //read .csv file on a server
         const res = await fetch(csvPedigreeUrl, {
@@ -28,34 +34,49 @@ const getCsv = async () => {
 }
 
 function createCsvFile(csv) {
-    fs.writeFile("./public/refreshDataAssets/pedigree.csv", csv, (err) => {
+    let fileNameAndLocation = fileOutputLocation + "flatPedigree.csv"
+    fs.writeFile(fileNameAndLocation, csv, (err) => {
         if (err) throw err;
         else {
-            console.log("pedigree.csv was created! it can be found at ./public/refreshDataAssets/pedigree.csv");
+            console.log("pedigree.csv was created! it can be found at " + fileNameAndLocation);
         }
     })
 }
 
 function createFlatJsonFile(json) {
-    fs.writeFile("./public/refreshDataAssets/flatPedigree.json", JSON.stringify(json), (err) => {
+    let fileNameAndLocation = fileOutputLocation + "flatPedigree.json"
+    fs.writeFile(fileNameAndLocation, JSON.stringify(json), (err) => {
         if (err) throw err;
         else {
-            console.log("flatPedigree.json was created! it can be found at ./public/refreshDataAssets/flatPedigree.json");
+            console.log("flatPedigree.json was created! it can be found at " + fileNameAndLocation);
         }
     })
 }
 
 // saves the output json to the src directory
 function createJsonFile(json) {
-    fs.writeFile("./src/pedigree.json", JSON.stringify(json, null, 3), (err) => {
+    let fileNameAndLocation = fileOutputLocation + "pedigree.json"
+    fs.writeFile(fileNameAndLocation, JSON.stringify(json, null, 3), (err) => {
         if (err) throw err;
         else {
-            console.log("pedigree.json was created! it can be found at ./src/pedigree.json");
+            console.log("pedigree.json was created! it can be found at " + fileNameAndLocation);
+        }
+    })
+}
+
+// saves the treeInfo json to the src directory
+function createTreeInfoJsonFile(json) {
+    let fileNameAndLocation = fileOutputLocation + "treeInfo.json"
+    fs.writeFile(fileNameAndLocation, JSON.stringify(json), (err) => {
+        if (err) throw err;
+        else {
+            console.log("treeInfo.json was created! it can be found at " + fileNameAndLocation);
         }
     })
 }
 
 function TreeNode(element) {
+    this.name = element["Registered Name"];
     this.attributes = {
         registrationNum: element["Registration #"],
         registrationType: element["Registration Type"],
@@ -66,65 +87,78 @@ function TreeNode(element) {
         chicNum: element["CHIC #"],
         hips: element["Hips"],
         ofaLink: element["OFA Link"],
-        offspringRegistrationNum: element["Child Reg #"]
+        parent1RegistrationNum: element["Mother Reg #"],
+        parent2RegistrationNum: element["Father Reg #"]
     };
     this.children = [];
-    this.name = element["Registered Name"];
 }
 
-function createTree(node, parentMap) {
-    if (node.attributes == undefined) {
+function createTree(node, dogMap) {
+    // count the total number of nodes in the tree
+    treeInfo.totalNumNodes++
+
+    if (node.attributes != undefined) {
+        let parent1RegNum = node.attributes.parent1RegistrationNum
+        let parent2RegNum = node.attributes.parent2RegistrationNum
+        let hasAtLeastOneParent = (parent1RegNum != "Unknown") && (parent2RegNum != "Unknown")
+        if (hasAtLeastOneParent) {
+            // add the current node and its child nodes (mother and father) to the tree 
+            if (dogMap.has(parent1RegNum)) {
+                let parent1 = dogMap.get(parent1RegNum)
+                node.children.push(new TreeNode(parent1))
+            }
+            if (dogMap.has(parent2RegNum)) {
+                let parent2 = dogMap.get(parent2RegNum)
+                node.children.push(new TreeNode(parent2))
+            }
+            if (node.children.length > 0) {
+                node.children.forEach((child) => { createTree(child, dogMap) })
+            }
+        } else {
+            // return, because that means that the current dog does not have known parents  
+            return node
+        }
+
+        // if (dogMap.has(node.attributes.registrationNum)) {
+        //     // let parents = dogMap.get(node.attributes.registrationNum)
+        //     if (parents[0]["Registration #"] != undefined) {
+        //         node.children.push(new TreeNode(parents[0]))
+        //     }
+        //     if (parents[1]["Registration #"] != undefined) {
+        //         node.children.push(new TreeNode(parents[1]))
+        //     }
+        //     if (node.children.length > 0) {
+        //         node.children.forEach((child) => { createTree(child, dogMap) })
+        //     }
+        // } else {
+        //     // return, because that means that the current node does not have parent nodes  
+        //     return node
+        // }
+    } else {
         // attempt to catch a random error that was showing up?
         console.log("node.attributes is undefined for " + node.name)
-        return node
-    }
-    if (parentMap.has(node.attributes.registrationNum)) {
-        let parents = parentMap.get(node.attributes.registrationNum)
-        if (parents[0]["Registration #"] != undefined) {
-            node.children.push(new TreeNode(parents[0]))
-        }
-        if (parents[1]["Registration #"] != undefined) {
-            node.children.push(new TreeNode(parents[1]))
-        }
-        if (node.children.length > 0) {
-            node.children.forEach((child) => { createTree(child, parentMap) })
-        }
-    } else {
-        // return, because that means that the current node does not have parent nodes  
         return node
     }
 }
 
 function createHierarchalJson(flatJson) {
-    var root;
-    let parentMap = new Map();
-
     // create a map from the flat json
-    //      key: offspring reg # 
-    //      value: an array that holds both dogs with that offspring reg #
-    flatJson.forEach(element => {
-        // if its child reg # is "None", create a root node object with that item's data
-        if (element['Child Reg #'] == "None") {
-            root = new TreeNode(element);
-        } else { // else, check if the child reg # is already in the map
-            if (parentMap.has(element['Child Reg #'])) {
-                // if it is, insert the object at map[childRegNum][1]
-                let parent1 = parentMap.get(element['Child Reg #']);
-                let parent2 = element;
-                let parentArray = [parent1, parent2]
-                parentMap.set(element['Child Reg #'], parentArray)
-            } else {
-                // if it isn't, insert the object at map[childRegNum][0]
-                parentMap.set(element['Child Reg #'], element)
-            }
-        }
+    //      key: registration # 
+    //      value: the dog's data as an object
+    let dogMap = new Map();
+    flatJson.forEach(dog => {
+        dogMap.set(dog['Registration #'], dog)
+        // count how many rows of data are processed
+        treeInfo.numDataRowsProcessed++
     })
 
     // create the tree structure by calling a recursive method on the root var
-    createTree(root, parentMap);
+    const gilsRegistrationNum = 'PR23772310'
+    let root = new TreeNode(dogMap.get(gilsRegistrationNum))
+    createTree(root, dogMap)
 
     // save the tree structure to a file
-    createJsonFile(root);
+    createJsonFile(root)
 }
 
 async function main() {
@@ -134,11 +168,15 @@ async function main() {
         .then(csv => { createCsvFile(csv); })
         // convert the csv file to a flat json array, then save that to a json file
         .then(() => {
-            csvToJson().fromFile("./public/refreshDataAssets/pedigree.csv")
+            csvToJson().fromFile("./src/refreshedData/current/flatPedigree.csv")
                 .then(json => {
                     createFlatJsonFile(json);
                     createHierarchalJson(json);
-                });
+                })
+                // save other info about the data, like how many total nodes and the depth 
+                .then(() => {
+                    createTreeInfoJsonFile(treeInfo);
+                })
         })
 }
 
